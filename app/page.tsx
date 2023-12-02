9e9384697b0f66f1bdadc0007b926b1474ac02c9
Sarah Gu <guwen888@gmail.com>
2023-12-01 20:57:53 -0500

    Phone friendly

--- app/page.tsx
+++ app/page.tsx
@@ -31,11 +31,11 @@ export default function Home() {
 
   	return (
 		<div className = "flex flex-col w-screen h-screen justify-center items-center bg-gradient-to-r from-blue-200 via-green-200 to-purple-300 backdrop-blur-3xl p-4">
-			<h1 className = "text-7xl font-bold pb-4">FortuneGPT</h1>
+			<h1 className = "text-5xl lg:text-7xl font-bold pb-4">FortuneGPT</h1>
 			<p className="pb-8">Ask a yes or no question</p>
 			<div className = "flex flex-row justify-center items-center space-x-4 p-8">
-				<input onChange={e => setPrompt(e.target.value)} className = "w-72 h-16 border-1 p-4 border border-black rounded-lg" placeholder="Is Sarah really smart?"></input>
-				<button type="button" onClick={handleSubmit} className = "w-24 h-16 border-1 p-4 border border-black rounded-lg bg-gray-200">Ask</button>
+				<input onChange={e => setPrompt(e.target.value)} className = "w-48 lg:w-72 h-16 border-1 p-4 border border-black rounded-lg" placeholder="Is Sarah really smart?"></input>
+				<button type="button" onClick={handleSubmit} className = "w-12 lg:w-24 h-16 flex items-center justify-center border-1 p-4 border border-black rounded-lg bg-gray-200">Ask</button>
 			</div>
 
 			{apiResponse && (

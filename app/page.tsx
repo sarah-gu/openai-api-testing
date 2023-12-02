f4da029c7527150dddb98db445b804b8bffabc58
Sarah Gu <guwen888@gmail.com>
2023-12-01 20:52:03 -0500

    resizing for phone

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,6 @@
 "use client"; 
 
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import {openai} from "./openai"; 
-
-// const fetchOpenAIResponse = async ({prompt, setApiResponse, setLoading}: {prompt: string, setApiResponse: Dispatch<SetStateAction<string>>, setLoading: Dispatch<SetStateAction<boolean>>}) => {
-// 	try {
-// 	//   const result = await openai.chat.completions.create({
-// 	// 	messages: [{"role": "system", "content": "You are a helpful assistant."},
-//     //     {"role": "user", "content": "Did the Los Angeles Dodgers win the world series in 2020?"},
-//     //     {"role": "assistant", "content": "Yes."},
-//     //     {"role": "user", "content": prompt}],
-//     // 	model: "gpt-4",
-// 	//   });
-
-// 	  console.log(result); 
-// 	  setApiResponse(result.data[0].url ?? "error");
-// 	} catch (error) {
-// 	  // Handle the error
-// 	  console.error(error);
-// 	  setApiResponse("Something is going wrong. Please try again.");
-// 	}
-// 	setLoading(false);
-//    };
+import { useState } from "react";
 
 export default function Home() {
 	const [prompt, setPrompt] = useState("");
@@ -51,7 +30,7 @@ export default function Home() {
 	  };
 
   	return (
-		<div className = "flex flex-col w-screen h-screen justify-center items-center bg-gradient-to-r from-blue-200 via-green-200 to-purple-300 backdrop-blur-3xl">
+		<div className = "flex flex-col w-screen h-screen justify-center items-center bg-gradient-to-r from-blue-200 via-green-200 to-purple-300 backdrop-blur-3xl p-4">
 			<h1 className = "text-7xl font-bold pb-4">FortuneGPT</h1>
 			<p className="pb-8">Ask a yes or no question</p>
 			<div className = "flex flex-row justify-center items-center space-x-4 p-8">
